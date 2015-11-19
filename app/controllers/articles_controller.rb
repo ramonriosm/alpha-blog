8234b98548dc678b5d222630c7a12a40752fdfe7
ramonriosm <ramonriosm@gmail.com>
2015-11-19 23:42:26 +0000

    create edit update template

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,17 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+  
   def new
     @article = Article.new
   end
 
   def show
+    @article = Article.find(params[:id])
+  end
+
+  def edit
     @article = Article.find(params[:id])
   end
 
@@ -16,6 +24,18 @@ class ArticlesController < ApplicationController
       redirect_to article_path(@article)
     else
       render 'new'
+    end
+
+  end
+
+  def update
+
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was succesfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
     end
 
   end
